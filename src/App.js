78e074eb94eb575c9634d06d5ace94c4e2ca0cb7
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-24 22:25:39 +0530

    installed material ui packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Landing from "./component/layout/Landing";
+import Login from "./component/auth/Login";
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello !</h1>
+    <div>
+      <Landing />
+      <Login />
     </div>
   );
 }
