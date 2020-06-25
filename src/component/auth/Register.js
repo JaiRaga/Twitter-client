25f767900f3a2cdfc90af17d2b46242c301115cb
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-25 22:12:35 +0530

    refactored register component

---
+++ src/component/auth/Register.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+  makeStyles,
+  Grid,
+  TextField,
+  Button,
+  Typography
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch"
+    }
+  },
+  registerContainer: {
+    marginTop: "20px"
+  }
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  return (
+    <Grid container justify='center' alignItems='center'>
+      <Grid
+        className={classes.registerContainer}
+        container
+        item
+        xs={6}
+        direction='column'
+        justify='center'
+        alignItems='center'>
+        <Grid item>
+          <Typography variant='h5'>Register</Typography>
+        </Grid>
+        <Grid
+          container
+          item
+          display='column'
+          justify='center'
+          alignItems='center'>
+          <form className={classes.root}>
+            <Grid item>
+              <TextField
+                id='outlined-basic'
+                label='Username'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item>
+              <TextField id='outlined-basic' label='Email' variant='outlined' />
+            </Grid>
+            <Grid item>
+              <TextField
+                id='outlined-basic'
+                label='handle'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id='outlined-basic'
+                label='password'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id='outlined-basic'
+                label='re-enter password'
+                variant='outlined'
+              />
+            </Grid>
+          </form>
+        </Grid>
+        <Grid item>
+          <Button color='primary' variant='contained'>
+            Let's Get Started
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
