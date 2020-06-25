9b8fca32b6b5cadc749c1ad541fc2789da1abe5a
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-25 22:15:53 +0530

    created tweets component to display twets

---
+++ src/component/tweets/Tweets.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const Tweets = () => {
+  return (
+    <Grid container item justify='center'>
+      tweets
+    </Grid>
+  );
+};
+
+export default Tweets;
