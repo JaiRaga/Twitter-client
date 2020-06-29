356ec5eee4b295d93abaed34b5c8e06af1884060
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-29 20:26:15 +0530

    created reducer for auth action creators

---
+++ src/Redux/reducers/auth.js
@@ -0,0 +1,56 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  USER_LOADED,
+  CLEAR_PROFILE,
+  AUTH_ERROR
+} from "../actions/types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  user: null,
+  isAuthenticated: null,
+  loading: false
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        user: payload,
+        isAuthenticated: true,
+        loading: false
+      };
+
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        token: payload.token,
+        isAuthenticated: true,
+        loading: false
+      };
+
+    case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT:
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+
+    default:
+      return state;
+  }
+};
