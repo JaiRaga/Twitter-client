1ad6ed29ffaae52cee7da76b2196ea36afaf8412
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-29 20:29:05 +0530

    provided the store to the app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
+// Components
+import Navbar from "./component/layout/Navbar";
 import Landing from "./component/layout/Landing";
 import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
+import ProfileContainer from "./component/profile/ProfileContainer";
+import Dashboard from "./component/dashboard/Dashboard";
+import Profiles from "./component/Profiles/Profiles";
+
+// Redux
+import store from "./Redux/store";
+import { loadUser } from "./Redux/actions/auth";
+
+// utils
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div>
-      <Landing />
-      <Login />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/Login' component={Login} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
