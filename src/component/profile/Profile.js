c4ee6e454ab1c7ef2ec14d494f0a36eb0c27dc97
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-30 20:33:27 +0530

    displayed data from the backend

--- src/component/profile/Profile.js
+++ src/component/profile/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Grid, Avatar, makeStyles, Button } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import { PacmanLoader } from "react-spinners";
 import profilePic from "../../img/raga.jpg";
 
 const useStyles = makeStyles((theme) => ({
@@ -25,6 +27,10 @@ const useStyles = makeStyles((theme) => ({
     margin: 9,
     padding: 3
   },
+  loadingSpinner: {
+    margin: 15,
+    padding: 15
+  },
   follow: {
     margin: 1
   }
@@ -32,34 +38,47 @@ const useStyles = makeStyles((theme) => ({
 
 const Profile = () => {
   const classes = useStyles();
+  const loading = useSelector((state) => state.auth.loading);
+  const user = useSelector((state) => state.auth.user);
+
   return (
     <Grid container item>
-      <Grid container direction='column' item className={classes.back}>
-        <Grid item>
-          <div className={classes.root}>
-            <Avatar
-              alt='username'
-              src={profilePic}
-              className={classes.large}></Avatar>
-          </div>
-          <Button variant='outlined'>Edit Profile</Button>
+      {loading || user === null ? (
+        <Grid
+          container
+          justify='center'
+          item
+          className={classes.loadingSpinner}>
+          <PacmanLoader loading color='#07ADEE' size={25} />
         </Grid>
-        <Grid container direction='column'>
-          <Grid className={classes.centerText} item>
-            username
+      ) : (
+        <Grid container direction='column' item className={classes.back}>
+          <Grid item>
+            <div className={classes.root}>
+              <Avatar
+                alt={user.username}
+                src={profilePic}
+                className={classes.large}></Avatar>
+            </div>
+            <Button variant='outlined'>Edit Profile</Button>
           </Grid>
-          <Grid className={classes.centerText} item>
-            handle
-          </Grid>
-          <Grid className={classes.centerText} item>
-            caption
-          </Grid>
-          <Grid container className={classes.follow} spacing={2} item>
-            <Grid item>{103} following</Grid>
-            <Grid item>{98} followers</Grid>
+          <Grid container direction='column'>
+            <Grid className={classes.centerText} item>
+              {user.username}
+            </Grid>
+            <Grid className={classes.centerText} item>
+              @{user.handle}
+            </Grid>
+            <Grid className={classes.centerText} item>
+              {user.caption}
+            </Grid>
+            <Grid container className={classes.follow} spacing={2} item>
+              <Grid item>{user.following.length} Following</Grid>
+              <Grid item>{user.followers.length} Followers</Grid>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      )}
     </Grid>
   );
 };
