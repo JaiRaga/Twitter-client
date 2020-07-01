c3c9badeeaf5556a7dcf5887e3434113b662517f
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-01 20:02:01 +0530

    added loading spinner

--- src/component/Profiles/Profiles.js
+++ src/component/Profiles/Profiles.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
 import ProfileItem from "./ProfileItem";
 import { Grid, Typography, IconButton, Button } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { SyncLoader } from "react-spinners";
 import Followers from "./Followers";
 import Following from "./Following";
 
+const style = {
+  Layout: {
+    marginLeft: 27
+  },
+  Spinner: {
+    margin: "10%"
+  }
+};
+
 const Profiles = () => {
+  const loading = useSelector((state) => state.auth.loading);
+  const user = useSelector((state) => state.auth.user);
   return (
     <Grid container>
-      <Grid item>
-        <IconButton>
+      <Grid item style={style.Layout}>
+        <IconButton aria-label='back-button' color='secondary'>
           <ArrowBackIcon />
         </IconButton>
       </Grid>
       <Grid item>
-        <Grid container item direction='column'>
+        <Grid container item direction='column' style={style.Layout}>
           <Grid item>
             <Typography variant='h5'>username</Typography>
           </Grid>
           <Grid item>
-            <Typography variant='h5'>handle</Typography>
+            <Typography variant='caption'>@handle</Typography>
           </Grid>
         </Grid>
       </Grid>
@@ -29,10 +42,19 @@ const Profiles = () => {
           <Following />
         </Grid>
         <Grid container item justify='center'>
-          <ProfileItem />
-          <Grid item>
-            <Button color='primary'>Follow</Button>
-          </Grid>
+          {loading || user === null ? (
+            <Grid item style={style.Spinner}>
+              <SyncLoader loading size={15} color='#36D7B7' />
+            </Grid>
+          ) : (
+            <Fragment>
+              <ProfileItem user={user} />
+              <ProfileItem user={user} />
+              <ProfileItem user={user} />
+              <ProfileItem user={user} />
+              <ProfileItem user={user} />
+            </Fragment>
+          )}
         </Grid>
       </Grid>
     </Grid>
