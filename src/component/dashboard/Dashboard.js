c6da74a2a363c4b42120af3e6a84406e38ded7d8
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-07 21:06:04 +0530

    changed margin

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -12,7 +12,15 @@ const style = {
 const Dashboard = () => {
   return (
     <Grid container justify='center' alignItems='center' style={style.Margin}>
-      <Grid container direction='column' item sm={10}>
+      <Grid
+        container
+        direction='column'
+        spacing={2}
+        item
+        xs={12}
+        sm={8}
+        md={10}
+        lg={5}>
         <Tweet />
         <Tweets />
       </Grid>
