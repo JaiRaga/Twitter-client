8cc13414ddffb0eaab89deabe13574fe5a245a38
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-13 20:01:55 +0530

    added dispatch to get tweets

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import { CircleLoader } from "react-spinners";
 import Tweets from "../tweets/Tweets";
 import Tweet from "../tweet/Tweet";
+import { getAllTweets } from "../../Redux/actions/tweet";
 
 const style = {
   Margin: {
@@ -10,19 +13,27 @@ const style = {
 };
 
 const Dashboard = () => {
+  // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const tweet = useSelector((state) => state.tweet);
+  const { loading, tweets, error } = tweet;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllTweets());
+  }, [tweets]);
+
+  console.log(tweet);
+
   return (
     <Grid container justify='center' alignItems='center' style={style.Margin}>
-      <Grid
-        container
-        direction='column'
-        spacing={2}
-        item
-        xs={12}
-        sm={8}
-        md={10}
-        lg={5}>
+      <Grid container direction='column' spacing={2} item xs={12} sm={8} md={5}>
         <Tweet />
-        <Tweets />
+        {loading || !tweets ? (
+          <CircleLoader loading />
+        ) : (
+          <Tweets tweets={tweets} />
+        )}
       </Grid>
     </Grid>
   );
