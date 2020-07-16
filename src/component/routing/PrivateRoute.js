4e969bbc79d37ee1e2ac02ed510b754df6002d60
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-16 20:25:09 +0530

    refactored code

--- src/component/routing/PrivateRoute.js
+++ src/component/routing/PrivateRoute.js
@@ -3,9 +3,8 @@ import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const auth = useSelector((state) => state.auth);
-  const { loading, isAuthenticated } = auth;
-
+  const { loading, isAuthenticated } = useSelector((state) => state.auth);
+  // console.log(rest);
   return (
     <Route
       {...rest}
