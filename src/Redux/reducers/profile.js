1ab17d2d9f9128cd8472576ac28154ff4c94ed45
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-17 19:52:52 +0530

    created profile reducer for storing info related to profile

---
+++ src/Redux/reducers/profile.js
@@ -0,0 +1,31 @@
+import { GET_PROFILE, CLEAR_PROFILE } from "../actions/types";
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  loading: true,
+  error: {}
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        loading: false
+      };
+
+    default:
+      return state;
+  }
+};
