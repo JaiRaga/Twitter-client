6d889e2cd8fcc315e7af0e84dd27b1177429ad6e
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-29 21:24:48 +0530

    tweaking code

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Grid } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import { useSelector, useDispatch } from "react-redux";
 import { CircleLoader } from "react-spinners";
 import Tweets from "../tweets/Tweets";
 import Tweet from "../tweet/Tweet";
-import { getAllTweets } from "../../Redux/actions/tweet";
+import { getAllTweets, clearTweets } from "../../Redux/actions/tweet";
 
 const style = {
   Margin: {
     marginTop: 10
+  },
+  Loading: {
+    marginBottom: 10
   }
 };
 
 const Dashboard = () => {
-  // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const tweet = useSelector((state) => state.tweet);
   const { loading, tweets, error } = tweet;
 
@@ -22,16 +26,28 @@ const Dashboard = () => {
   useEffect(() => {
     dispatch(getAllTweets());
     console.log("dash");
-  }, [loading]);
-
-  // console.log(tweet);
+  }, []);
 
   return (
     <Grid container justify='center' alignItems='center' style={style.Margin}>
       <Grid container direction='column' spacing={2} item xs={12} sm={8} md={5}>
         <Tweet />
-        {loading || !tweets ? (
-          <CircleLoader loading />
+        {loading || !tweets.length ? (
+          <Fragment>
+            <Skeleton
+              variant='rect'
+              style={style.Loading}
+              animation='wave'
+              height={200}
+            />
+            <Skeleton variant='rect' style={style.Loading} height={200} />
+            <Skeleton
+              variant='rect'
+              style={style.Loading}
+              animation='wave'
+              height={200}
+            />
+          </Fragment>
         ) : (
           <Tweets tweets={tweets} />
         )}
