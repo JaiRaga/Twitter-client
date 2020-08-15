39d5d65e22fdbe11bc681fb311b5da35a54506f0
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-15 15:43:29 +0530

    created component to display who a user is following

--- src/component/Profiles/Following.js
+++ src/component/Profiles/Following.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Grid, IconButton, Button } from "@material-ui/core";
+import Profiles from "./Profiles";
+import { useSelector } from "react-redux";
 
 const Following = () => {
-  return <div>Following</div>;
+  const following = useSelector((state) => state.profile.following);
+  return (
+    <Grid item>
+      <Profiles profiles={following} />
+    </Grid>
+  );
 };
 
 export default Following;
