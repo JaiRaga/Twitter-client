b24b768016cfcce0298b0c68ac5a3af7d2dc140a
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-17 19:31:57 +0530

    added follow/unfollow feature

--- src/component/Profiles/ProfileItem.js
+++ src/component/Profiles/ProfileItem.js
@@ -9,29 +9,34 @@ import {
 } from "@material-ui/core";
 
 import { SyncLoader } from "react-spinners";
+import { useDispatch, useSelector } from "react-redux";
 import profilePic from "../../img/raga.jpg";
+import { followUser, unFollowUser } from "../../Redux/actions/profile";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     "& > *": {
       margin: theme.spacing(1)
     }
   },
   spaceLeft: {
-    // marginLeft: "auto"
+    marginLeft: "auto"
   }
 }));
 
 const ProfileItem = ({ user }) => {
   const classes = useStyles();
-
+  const dispatch = useDispatch();
+  const following = useSelector((state) => state.profile.following);
+  const isFollowing = following.filter((follow) => follow._id === user._id);
+  console.log(isFollowing);
   return (
     <Fragment>
-      <Grid container item>
+      <Grid container item justify='center'>
         <Grid item className={classes.root}>
-          <Avatar alt='username' src={profilePic} />
+          <Avatar alt='username' src={user.avatar} />
           <Grid container item direction='column' xs={5}>
             <Grid item>
               <Typography variant='h6'>{user.username}</Typography>
@@ -48,8 +53,28 @@ const ProfileItem = ({ user }) => {
               </Typography>
             </Grid>
           </Grid>
-          <Grid item>
-            <Button color='primary'>Follow</Button>
+          <Grid item className={classes.spaceLeft}>
+            {/* <Button
+              color='primary'
+              onClick={() =>
+                isFollowing
+                  ? dispatch(followUser(user._id))
+                  : dispatch(unFollowUser(user._id))
+              }></Button> */}
+            {isFollowing.length > 0 ? (
+              <Button
+                color='primary'
+                onClick={() => dispatch(unFollowUser(user._id))}>
+                Following
+              </Button>
+            ) : (
+              <Button
+                color='primary'
+                variant='contained'
+                onClick={() => dispatch(followUser(user._id))}>
+                Follow
+              </Button>
+            )}
           </Grid>
         </Grid>
       </Grid>
