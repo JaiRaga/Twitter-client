0633450f563817611bf007b1f80422ac9bd1d233
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-18 07:57:49 +0530

    Updates

---
+++ precache-manifest.bcc5a129004af5878ba7c581f9e61888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f4ee99945065e9dc7a2c86d4bdc22e",
+    "url": "/Twitter-client/index.html"
+  },
+  {
+    "revision": "893f77de64d532439b76",
+    "url": "/Twitter-client/static/css/main.acfa2fab.chunk.css"
+  },
+  {
+    "revision": "ef1e0e4500f131a32f1e",
+    "url": "/Twitter-client/static/js/2.66e1d1a5.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/Twitter-client/static/js/2.66e1d1a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893f77de64d532439b76",
+    "url": "/Twitter-client/static/js/main.c09711aa.chunk.js"
+  },
+  {
+    "revision": "a9b391f89e9b3944f35c",
+    "url": "/Twitter-client/static/js/runtime-main.30e99ab3.js"
+  },
+  {
+    "revision": "67a6b6428965fe9397c5cac16e4e1dbb",
+    "url": "/Twitter-client/static/media/raga.67a6b642.jpg"
+  }
+]);
